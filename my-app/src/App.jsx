531563b07c9777e6b6a5757345e92ca39907e369
jorge <chauxjorge@hotmail.com>
2026-02-06 17:27:35 -0500

    Add Creating reusable components with props in React

---
+++ my-app/src/App.jsx
@@ -0,0 +1,12 @@
+import Card from './components/Card'
+function App() {
+  
+  return (
+    <section>
+      <Card title="Card1" description="This is a description" />
+      <Card title="Card Title 2" description="Description 2" />
+    </section>
+  )
+}
+
+export default App
