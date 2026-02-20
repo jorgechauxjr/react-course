ea76eb750695c0a48ce93b69f331f4361a1fdc95
jorge <chauxjorge@hotmail.com>
2026-02-20 16:11:34 -0500

    [Add] 11 use tailwind css

--- my-react-app-styles/src/App.jsx
+++ my-react-app-styles/src/App.jsx
@@ -1,6 +1,7 @@
 import Card from "./components/Card/Card"
 import CardSecondary from "./components/Card/CardSecondary"
 import Button from "./components/Button/Button"
+import TailwindText from "./components/Text/Text"
 
 function App() {
   
@@ -10,6 +11,7 @@ function App() {
       <CardSecondary />
       <Button blue><p>Hola</p></Button>
       <Button><p>Gris</p></Button>
+      a<TailwindText />
     </>
   )
 }
---
+++ my-react-app-styles/src/components/Text/Text.jsx
@@ -0,0 +1,7 @@
+const TailwindText = () => {
+    return(
+        <h1 className="text-3xl font-bold underline bg-cyan-500"> Hello world! </h1>
+    );
+}
+
+export default TailwindText;
