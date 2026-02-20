71c48cf4f604ea1003a0581eba16fec49c3a8f57
jorge <chauxjorge@hotmail.com>
2026-02-20 11:18:27 -0500

    [Add] modules sass and scss

--- my-react-app-styles/src/components/Card/Card.jsx
+++ my-react-app-styles/src/components/Card/Card.jsx
@@ -1,5 +1,6 @@
 // import "./styles.css"
-import styles from "./Card.module.css"
+// import styles from "./Card.module.scss"
+import styles from "./Card.module.sass"
 
 const Card = () => {
     return (
