673890a2d3202e8d692440cbe2ea8bd2c4a69786
jorge <chauxjorge@hotmail.com>
2026-02-20 14:14:28 -0500

    [Add] 9 Styles in React, inline styles

--- my-react-app-styles/src/App.jsx
+++ my-react-app-styles/src/App.jsx
@@ -1,9 +1,12 @@
 import Card from "./components/Card/Card"
+import CardSecondary from "./components/Card/CardSecondary"
+
 function App() {
   
   return (
     <>
       <Card />
+      <CardSecondary />
     </>
   )
 }
---
+++ my-react-app-styles/src/components/Card/CardSecondary.jsx
@@ -0,0 +1,14 @@
+const CardSecondary = () => {
+    const textStyles = {
+        color: "blue",
+        fontSize: "2rem",
+        textAlign: "center"
+    }
+    return (
+        <div style={ textStyles }>
+            Soy una card 2!
+        </div>
+    )
+}
+
+export default CardSecondary;
