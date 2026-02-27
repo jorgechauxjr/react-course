4a0ac4d0ec1dc5f1a5d5362699cfbc16ae06bacd
jorge <chauxjorge@hotmail.com>
2026-02-27 11:13:15 -0500

    [Add] 13 ejemplo con loading y error

--- my-react-app-data/src/components/UserList/UserList.jsx
+++ my-react-app-data/src/components/UserList/UserList.jsx
@@ -2,13 +2,29 @@ import { useState, useEffect } from "react";
 
 const UserList = () => {
 const [users, setUsers] = useState([]);
+const [loading, setLoading] = useState(true);
+const [error, setError] = useState('');
+
 
 useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
-        .then(response => response.json())
-        .then(data => setUsers(data))
-        .catch(error => console.error("El error al hacer fetch es: ", error))
-}, [])
+        .then(response => {
+            if (!response.ok) throw new Error('Error en la respuesta');
+                return response.json();
+        })
+        .then(data => {
+            setUsers(data)
+            setLoading(false);
+        })
+        .catch(error => {
+            console.error("El error al hacer fetch es: ", error)
+            setError(error.message);
+            setLoading(false);
+        })
+}, []);
+
+if (loading) return <div>Cargando...</div>;
+if (error) return <div>{error}</div>;
 
 
     return (
