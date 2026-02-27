f0bc789c2fb52a0e856c4b2f253d7e412dd568a9
jorge <chauxjorge@hotmail.com>
2026-02-27 10:49:39 -0500

    [Add] 13 implement useEffect and fetch to bring users

--- my-react-app-data/src/components/UserList/UserList.jsx
+++ my-react-app-data/src/components/UserList/UserList.jsx
@@ -5,13 +5,20 @@ const [users, setUsers] = useState([]);
 
 useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
-        .then()
+        .then(response => response.json())
+        .then(data => setUsers(data))
+        .catch(error => console.error("El error al hacer fetch es: ", error))
 }, [])
 
     return (
-        <>
-            <p>USER LIST</p>
-        </>
+        <div>
+            <h1>Listas de usuarios</h1>
+            <ul>
+                {users.map((user) => (
+                    <li key={user.id}>{user.name}</li>
+                    ))}
+            </ul>
+        </div>
     );
 }
 
