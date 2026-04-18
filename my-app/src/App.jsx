f3aeeea0d06fe12ed1698721a5e15783b47e42ec
jorge <chauxjorge@hotmail.com>
2026-04-18 10:03:29 -0500

    [Add] 3 Props for reusable component

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -4,14 +4,34 @@ import NameForm from './components/NameForm'
 import ToggleButton from './components/ToggleButton'
 import CounterWithEffect from './components/CounterWithEffect'
 import List from './components/List'
+import CardCountry from './components/CardCountry'
 
 function App() {
   
+  const countries = [
+        { id: 1, name:"USA", description: "Country in North America" },
+        { id: 2, name:"Israel", description:"Country in Middle East" },
+        { id: 3, name:"Colombia", description: "Country in South America"},
+        { id: 4, name:"Mexico", description: "Country in Central America" },
+        { id: 5, name:"Germany", description: "Country in Europe" },
+        { id: 6, name:"Nigeria", description: "Country in Africa" }
+    ];
+
   return (
     <section>
-      <List />
+      {
+        countries.map(country => (
+          <CardCountry
+            key={country.id}
+            name={country.name}
+            description={country.description}
+          />
+        ))
+      }
+
       <Card title="Card1" description="This is a description" />
       <Card title="Card Title 2" description="Description 2" />
+      <List />
       <Counter />
       <ToggleButton />
       <NameForm />
---
+++ my-app/src/components/CardCountry.jsx
@@ -0,0 +1,11 @@
+const CardCountry = ({name, description}) => {
+return (
+    <>
+        <h2>Country: {name}</h2>
+        <p> Description: {description}</p>
+    </>
+)
+
+}
+
+export default CardCountry;
