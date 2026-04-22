e1a6f6d9f4442f4739bbe4f440d6bdd0becbfcad
jorge <chauxjorge@hotmail.com>
2026-04-22 15:02:33 -0500

    [Add] 17 context API: manejo de estados globales y proveedores. createContext() useContext()

--- my-react-app-advanced/src/App.jsx
+++ my-react-app-advanced/src/App.jsx
@@ -1,11 +1,51 @@
 import CounterAdvanced from "./components/CounterAdvanced/CounterAdvanced"
+import { createContext, useContext, useState } from 'react';
 
-function App() {
+// 1.Crear el contexto
+const ThemeContext = createContext();
+
+
+// 2.Implementar un proveedor
+function ThemeProvider({ children }) {
+  const [theme, setTheme] = useState('light');
   
+  const toggleTheme = () => {
+    setTheme(prevTheme => prevTheme === 'light' ? 'dark' : 'light');
+  };
 
   return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );  
+}
+
+// 3. Consumir el contexto desde los componentes
+// en este caso componente ThemeButton
+function ThemeButton() {
+  const { theme, toggleTheme } = useContext(ThemeContext);
+  
+  return (
+    <button 
+      onClick={toggleTheme}
+      style={{
+        backgroundColor: theme === 'light' ? '#ffffff' : '#333333',
+        color: theme === 'light' ? '#000000' : '#ffffff'
+      }}
+    >
+      Cambiar tema a { theme==='light' ? 'Dark' : 'Light' }
+    </button>
+  );
+}  
+
+function App() {
+  return (
     <>
-      <CounterAdvanced />
+      {/* <CounterAdvanced /> */}
+      <ThemeProvider>
+        <ThemeButton />
+        {/* Otros componentes */}
+    </ThemeProvider>
     </>
   )
 }
